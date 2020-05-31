ea7af9e4046f6cc57c476e85d1d5389d6cb9af04
Brad Temple <brad.temple@gmail.com>
2020-05-31 14:31:55 -0700

    heroku tweaks

--- index.js
+++ index.js
@@ -64,14 +64,8 @@ app.put('/api/notes/:id', (req, res, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res, next) => {
   const body = req.body
-
-  if (!body.content) {
-    return res.status(400).json({
-      error: 'content missing'
-    })
-  }
 
   const note = new Note({
     content: body.content,
@@ -79,9 +73,13 @@ app.post('/api/notes', (req, res) => {
     date: new Date()
   })
   
-  note.save().then(savedNote => {
-    res.json(savedNote)
-  })
+  note
+    .save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => {
+      res.json(savedAndFormattedNote)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (req, res) => {
@@ -95,6 +93,8 @@ const errorHandler = (error, req, res, next) => {
 
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
 
   next(error)
